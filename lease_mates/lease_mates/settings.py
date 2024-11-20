04cf79bf5a3af7d30065e7750d5efd8340e6b397
Fayyaz Zaidi <fzaidi6@gatech.edu>
2024-11-19 22:50:44 -0500

    postgres work

--- lease_mates/lease_mates/settings.py
+++ lease_mates/lease_mates/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'communication'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,15 @@ WSGI_APPLICATION = 'lease_mates.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lease_mates_db',
+        'USER': 'postgres',
+        'PASSWORD': 'FJN123!',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
+
 
 
 # Password validation
