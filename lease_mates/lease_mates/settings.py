738169a94bf310147b2b81ace37829dfbffe43a8
Fayyaz Zaidi <fzaidi6@gatech.edu>
2024-11-21 00:08:07 -0500

    basic jwt

--- lease_mates/lease_mates/settings.py
+++ lease_mates/lease_mates/settings.py
@@ -38,8 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'communication'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',  # Default to require authentication
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
